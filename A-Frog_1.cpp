b94057d964ebb842566594cbc9ed976afcf8767b
aman <amangoel9873572693@gmail.com>
2020-08-18 20:57:33 +0530

    Dynammic_Programming_Problem_1

---
+++ A-Frog_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+#define ll long long int
+
+int main(){
+    ll n;
+    cin>>n;
+    ll heights[n];
+    for(ll i=0;i<n;i++){
+        cin>>heights[i];
+    }
+
+    ll answer_array[n] = {0};
+    answer_array[0] = 0;
+    answer_array[1] = abs(heights[1] - heights[0]);
+    for(ll i=2;i<n;i++){
+        answer_array[i] = min(answer_array[i-1] + abs(heights[i] - heights[i-1]),answer_array[i-2] +abs(heights[i] - heights[i-2]));
+    }
+    cout<<answer_array[n-1]<<endl;
+}
