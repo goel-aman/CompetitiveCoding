fb38fbbbf58513a80d50de056d0e28dcf8b083d4
aman <amangoel9873572693@gmail.com>
2020-08-22 03:14:03 +0530

    code-added

--- Codeforces_Problems/CodeForces_Round_665/Mere_Array.cpp
+++ Codeforces_Problems/CodeForces_Round_665/Mere_Array.cpp
@@ -2,76 +2,41 @@
 using namespace std;
 #define ll long long int
 
-ll gcd(ll first, ll second){
-    if(first == 0 || second == 0){
-        return max(first,second);
-    }
-
-    ll x = max(first,second);
-    ll y = min(first,second);
-    return gcd(y,x%y);
-}
-
 int main(){
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio();
     cin.tie(NULL);
-    ll t; 
+    ll t;
     cin>>t;
     while(t--){
         ll n;cin>>n;
         ll arr[n];
+        ll arr2[n];
         ll mini = LLONG_MAX;
-        for(ll i=0;i<n;i++){
+        for(ll i=0;i<n;i++) {
             cin>>arr[i];
+            arr2[i] = arr[i];
             if(arr[i] < mini){
                 mini = arr[i];
             }
         }
-        if(n == 1){
-            cout<<"YES"<<endl;
-            continue;
-        }
+        sort(arr,arr+n);
         int flag = 1;
-        for(ll i=0;i<n-1;i++){
-            if(arr[i+1] >= arr[i]){
+        for(ll i=0;i<n;i++){
+            if(arr[i] == arr2[i]){
                 continue;
             }
             else{
-                flag = 0;
-                break;
-            }
-        }
-
-        if(flag == 1){
-            cout<<"YES"<<endl;
-            continue;
-        }
-
-        for(ll i=0;i<n;i++){
-            for(ll j=i+1;j<n;j++){
-                if(gcd(arr[j],arr[i]) == mini && arr[i] > arr[j]){
-                    ll temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if(arr2[i]%mini != 0){
+                    cout<<"NO"<<endl;
+                    flag = 0;
+                    break;
                 }
             }
+            
         }
-        int flag2 = 1;
-        for(ll i=0;i<n - 1;i++){
-            if(arr[i] <= arr[i+1]){
-                continue;
-            }
-            else{
-                flag2 = 0;
-                break;
-            }
-        }
-        if(flag2 == 0){
-            cout<<"NO"<<endl;
-        }
-        else{
+        if(flag == 1){
             cout<<"YES"<<endl;
-        }
+        }        
     }
     return 0;
 }
