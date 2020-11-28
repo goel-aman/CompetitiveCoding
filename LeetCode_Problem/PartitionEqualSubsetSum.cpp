844444e3a53d3006fc52b10e527bd61382d8f6c9
goel-aman <amangoel9873572693@gmail.com>
2020-11-28 15:01:54 +0530

    PartitionEqualSubsetSum.cpp

--- LeetCode_Problem/PartitionEqualSubsetSum.cpp
+++ LeetCode_Problem/PartitionEqualSubsetSum.cpp
@@ -1,33 +1,59 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
 class Solution {
-public:
-    bool ans(vector<int> &nums,int current_value,int index,int &ans){
-        if(current_value == ans){
+    vector<vector<int>> mem;
+    bool anss(vector<int> &nums,int sum,int index,int siz){
+        if(sum == 0){
             return true;
         }
+        
+        if(sum < 0){
+            return false;
+        }
+        
+        if(index == siz){
+            if(sum == 0){
+                return true;
+            }
+            return false;
+        }
+        
+        if(mem[sum][index] != -1){
+            return mem[sum][index];
+        }
+        
 
-        bool ans1 = ans(nums,current_value + nums[index],index + 1,ans);
-        bool ans2 = ans(nums,current_value,index + 1, ans);
+        bool ans1 = anss(nums,sum - nums[index],index + 1,siz);
+        bool ans2 = anss(nums,sum,index + 1,siz);
 
-        if(ans1 && ans2)
+        if(ans1 || ans2)
         {
+            mem[sum][index] = 1;
             return true;
         }
+        mem[sum][index] = 0;
         return false;
     }
+public:
+    
 
     bool canPartition(vector<int>& nums) {
         int totalsum = 0;
         for(auto k : nums){
             totalsum += k;
         }
+        
+        int siz = nums.size();
+        
         if(totalsum%2 != 0){
             return false;
         }
         int ans = totalsum / 2;
-        int index = 0;
-        if(ans(nums,0,index,ans)){
+        mem.clear();
+        mem.resize(ans+ 1, vector<int>(siz, -1));
+        if(anss(nums,ans,0,siz)){
             return true;
         }
         return false;
