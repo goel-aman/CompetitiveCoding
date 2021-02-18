5cede6af21f4a3ce2e9ebd3c5d5962dfbdb6a7de
goel-aman <amangoel9873572693@gmail.com>
2021-02-18 12:46:21 +0530

    HanSoloAndLazerGun.cpp

---
+++ Codeforces_Problems/HanSoloAndLazerGun.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+    int n,x,y;
+    cin>>n>>x>>y;
+    set<double> um;
+
+    while(n--){
+        int x1,y1;
+        cin>>x1>>y1;
+        if(x1 - x == 0){
+            um.insert((double) INT_MAX);
+            continue;
+        }
+
+        double slope = (double)((double)(y1 - y) / (double) (x1 - x));
+        // cout<<slope<<endl;
+        // cout<<"slope is : "<<slope<<endl;
+        um.insert(slope);
+    }
+    cout<<um.size()<<endl;
+}
+
