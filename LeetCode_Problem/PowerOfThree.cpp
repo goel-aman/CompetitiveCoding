9070c3bd0b38460c5ee32422a70fb2e15cd2ad46
goel-aman <amangoel9873572693@gmail.com>
2021-03-09 20:52:46 +0530

    PowerOfThree.cpp

---
+++ LeetCode_Problem/PowerOfThree.cpp
@@ -0,0 +1,47 @@
+// 326. Power of Three
+
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+// Example 1:
+// Input: n = 27
+// Output: true
+
+// Example 2:
+// Input: n = 0
+// Output: false
+
+// Example 3:
+// Input: n = 9
+// Output: true
+
+// Example 4:
+// Input: n = 45
+// Output: false
+ 
+// Constraints:
+// -231 <= n <= 231 - 1
+
+// Follow up: Could you solve it without loops/recursion?
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 0){
+            return false;
+        }
+        if(n == 1){
+            return true;
+        }
+        
+        while(n%3 == 0){
+            n = n / 3;
+        }
+        
+        if(n == 1){
+            return true;
+        }
+        return false;
+    }
+};
