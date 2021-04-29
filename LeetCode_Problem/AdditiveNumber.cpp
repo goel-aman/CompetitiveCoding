0bcf918137190a6bf99cd4410cc0ffa5e5e0e6c1
goel-aman <amangoel9873572693@gmail.com>
2021-04-29 13:31:48 +0530

    AdditiveNumber.cpp

---
+++ LeetCode_Problem/AdditiveNumber.cpp
@@ -0,0 +1,69 @@
+// 306. Additive Number
+
+// Additive number is a string whose digits can form additive sequence.
+// A valid additive sequence should contain at least three numbers. 
+// Except for the first two numbers, each subsequent number in the sequence
+//  must be the sum of the preceding two.
+
+// Given a string containing only digits '0'-'9', write a function to 
+// determine if it's an additive number.
+
+// Note: Numbers in the additive sequence cannot have leading zeros,
+//  so sequence 1, 2, 03 or 1, 02, 3 is invalid.
+
+// Example 1:
+// Input: "112358"
+// Output: true
+// Explanation: The digits can form an additive sequence: 1, 1, 2, 3, 5, 8. 
+//              1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
+
+// Example 2:
+// Input: "199100199"
+// Output: true
+// Explanation: The additive sequence is: 1, 99, 100, 199. 
+//              1 + 99 = 100, 99 + 100 = 199
+ 
+// Constraints:
+// num consists only of digits '0'-'9'.
+// 1 <= num.length <= 35
+// Follow up:
+// How would you handle overflow for very large input integers?
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAdditiveNumber(string num) {
+        int numLength = num.length();
+
+        for(int i = 1;i < numLength;i++){
+            string s1 = num.substr(0,i);
+            if(s1.size() > 1 && s1[0] == '0'){
+                break;
+            }
+            for(int j = i + 1;j<numLength;j++){
+                string s2 = num.substr(i,j - i);
+                if(s2.size() > 1 && s2[0] == '0'){
+                    break;
+                }
+                long first = stol(s1);
+                long second = stol(s2);
+                long next = first + second;
+                string nextString = to_string(next);
+                string allString = s1 + s2 + nextString;
+                while(allString.length() < numLength){
+                    first = second;
+                    second = next;
+                    next = first + second;
+                    nextString = to_string(next);
+                    allString += nextString;
+                }
+                if(allString == num){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
--- LeetCode_Problem/DeleteColumnsToMakeSorted3.cpp
+++ LeetCode_Problem/DeleteColumnsToMakeSorted3.cpp
@@ -63,5 +63,4 @@ public:
         }
         return res;
     }
-
 };
