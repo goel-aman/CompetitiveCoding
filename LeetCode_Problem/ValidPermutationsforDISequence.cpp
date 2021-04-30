6e7ae142539682e38a662bd06f4cd74852644085
goel-aman <amangoel9873572693@gmail.com>
2021-04-30 07:54:05 +0530

    FindPositiveIntegerSolutionForAGivenEquation.cpp

---
+++ LeetCode_Problem/ValidPermutationsforDISequence.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numSequence(string s,int used,int sLength){
+
+    }
+
+    int numPermsDISequence(string S) {
+        int sLength = S.length();
+        int used = 0;
+        return numSequence(s,used,sLength);
+    }
+};
