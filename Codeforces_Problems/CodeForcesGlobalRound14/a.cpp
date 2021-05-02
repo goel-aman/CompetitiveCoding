5e5578d70ed539b3c7d44000a06f90963a178c77
goel-aman <amangoel9873572693@gmail.com>
2021-05-03 00:04:02 +0530

    KokoEatingBananas.cpp

--- Codeforces_Problems/CodeForcesGlobalRound14/a.cpp
+++ Codeforces_Problems/CodeForcesGlobalRound14/a.cpp
@@ -6,7 +6,45 @@ int main(){
     ll t;
     cin>>t;
     while(t--){
-        ll n; cin>>n;
+        ll n,x; cin>>n>>x;
+        ll weights[n];
+        for(int i=0;i<n;i++){
+            cin>>weights[i];
+        }
+        sort(weights,weights + n);
+        reverse(weights,weights + n);
+        int start = 0, end = n - 1;
+        int ans = 0;
+        int isPossible = true;
+        vector<int> an;
+        while(start <= end){
+            if(ans + weights[start] != x){
+                ans = ans + weights[start];
+                an.push_back(weights[start]);
+                start++;
+                continue;
+            }
+
+            if(ans + weights[end] != x){
+                ans = ans + weights[end];
+                an.push_back(weights[end]);
+                end--;
+                
+                continue;
+            }
+
+            isPossible = false;
+            break;
+        }
+
+        if(isPossible == false){
+            cout<<"NO"<<endl;continue;
+        }
+        cout<<"YES"<<endl;
+        for(auto element : an){
+            cout<<element<<" ";
+        }
+        cout<<endl;
     }
     return 0;
 }
--- Codeforces_Problems/CodeForcesGlobalRound14/b.cpp
+++ Codeforces_Problems/CodeForcesGlobalRound14/b.cpp
@@ -5,8 +5,41 @@ using namespace std;
 int main(){
     ll t;
     cin>>t;
+    int gotAnswer = false; 
     while(t--){
         ll n; cin>>n;
+        if(n%2 != 0){
+            cout<<"NO"<<endl;
+            continue;
+        }
+        int power2 = 0;
+        while(true){
+            while(n%2 == 0){
+                n = n / 2;
+                power2++;
+            }
+
+            int val = sqrt(n);
+            // cout<<"value of val is : "<<val<<endl;
+            if(power2 == 0){
+                cout<<"NO"<<endl;
+                continue;
+            }
+            // cout<<"hello"<<endl;
+            if((double) (val * val) == n){
+                // cout<<"hello"<<endl;
+                gotAnswer = true;
+            }
+            else{
+                gotAnswer = false;
+            }
+            break;
+        }
+        if(gotAnswer == true){
+            cout<<"YES"<<endl;
+            continue;
+        }
+        cout<<"NO"<<endl;
     }
     return 0;
 }
