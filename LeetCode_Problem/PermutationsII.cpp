4c7a6962247954c4f300c442255f60cbe09279b2
goel-aman <amangoel9873572693@gmail.com>
2021-05-24 00:40:51 +0530

    permutations2.cpp

--- LeetCode_Problem/PermutationsII.cpp
+++ LeetCode_Problem/PermutationsII.cpp
@@ -21,8 +21,44 @@ using namespace std;
 // -10 <= nums[i] <= 10
 
 class Solution {
+    set<vector<int>> s;
+    int length;
 public:
+    void solve(vector<int>& nums,vector<int>& visited,vector<int>& res){
+        bool allVisited = true;
+        for(int i=0;i<length;i++){
+            if(visited[i] == 0){
+                allVisited = false;
+                break;
+            }
+        }
+
+        if(allVisited){
+            s.insert(res);
+            return;
+        }
+
+        for(int i=0;i<length;i++){
+            if(visited[i] == 0){
+                visited[i] = 1;
+                res.push_back(nums[i]);
+                solve(nums,visited,res);
+                res.pop_back();
+                visited[i] = 0;
+            }
+        }
+        return ;
+    }
+
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        
+        length = nums.size();
+        vector<int> res;
+        vector<int> visited(length,0);
+        solve(nums,visited,res);
+        vector<vector<int>> ret;
+        for(auto element : s){
+            ret.push_back(element);
+        }
+        return ret;
     }
 };
