122b0c9f7da35303feb2fb853a5a03c8a19fe0f2
goel-aman <amangoel9873572693@gmail.com>
2021-05-24 14:26:15 +0530

    HourseRobber.cpp

---
+++ LeetCode_Problem/HouseRobber.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 198. House Robber
+
+// You are a professional robber planning to rob houses along a street. 
+// Each house has a certain amount of money stashed, the only constraint 
+// stopping you from robbing each of them is that adjacent houses have 
+// security systems connected and it will automatically contact the police
+//  if two adjacent houses were broken into on the same night.
+
+// Given an integer array nums representing the amount of money of each house,
+//  return the maximum amount of money you can rob tonight without alerting the police.
+
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: 4
+// Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+// Total amount you can rob = 1 + 3 = 4.
+
+// Example 2:
+// Input: nums = [2,7,9,3,1]
+// Output: 12
+// Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+// Total amount you can rob = 2 + 9 + 1 = 12.
+
+// Constraints:
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 400
+
+class Solution {
+    int length;
+    unordered_map<int,int> um;
+public:
+    int maxRob(vector<int>& nums,int index){
+        if(index >= length){
+            return 0;
+        }
+        if(um.count(index)){
+            return um[index];
+        }
+        int value = 0, value2 = 0;
+        // including current index
+        value = nums[index] + maxRob(nums,index + 2);
+
+        // excluding current index
+        value2 = maxRob(nums,index + 1);
+
+        return um[index] = max(value,value2);
+    }
+
+    int rob(vector<int>& nums) {
+        length = nums.size();
+        return maxRob(nums,0);      
+    }
+};
