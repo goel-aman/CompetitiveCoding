cc65270b8abfe04c778f3a142e78de4924251b74
goel-aman <amangoel9873572693@gmail.com>
2021-05-26 14:34:11 +0530

    Triangle.cpp

---
+++ LeetCode_Problem/Triangle.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 120. Triangle
+// Given a triangle array, return the minimum path sum from top to bottom.
+// For each step, you may move to an adjacent number of the row below. More 
+// formally, if you are on index i on the current row, you may move to either
+//  index i or index i + 1 on the next row.
+
+// Example 1:
+// Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+// Output: 11
+// Explanation: The triangle looks like:
+//    2
+//   3 4
+//  6 5 7
+// 4 1 8 3
+// The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11
+//  (underlined above).
+
+// Example 2:
+// Input: triangle = [[-10]]
+// Output: -10
+ 
+// Constraints:
+// 1 <= triangle.length <= 200
+// triangle[0].length == 1
+// triangle[i].length == triangle[i - 1].length + 1
+// -104 <= triangle[i][j] <= 104
+ 
+// Follow up: Could you do this using only O(n) extra space,
+//  where n is the total number of rows in the triangle?
+
+class Solution {
+    int rows;
+    int dp[201][206];
+public:
+    int solve(vector<vector<int>>& triangle,int col,int row){
+        if(row >= rows){
+            return 0;
+        }
+        
+        if(col >= triangle[row].size()){
+            return 0;
+        }
+        if(dp[row][col] != -1){
+            return dp[row][col];
+        }
+        
+        int ans = 0;
+        ans += triangle[row][col];
+
+        ans += min(solve(triangle,col,row + 1), solve(triangle,col + 1, row + 1));
+        return dp[row][col] = ans;      
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        rows = triangle.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(triangle,0,0);
+    }
+};
