4a7b2a428b389670a8402e4753eaf2f5abd48109
goel-aman <amangoel9873572693@gmail.com>
2021-06-28 14:50:04 +0530

    reverseWordsInAString.cpp

---
+++ LeetCode_Problem/ReverseWordsInAString.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 151. Reverse Words in a String
+
+// Given an input string s, reverse the order of the words.
+// A word is defined as a sequence of non-space characters.
+//  The words in s will be separated by at least one space.
+// Return a string of the words in reverse order concatenated
+//  by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple
+// spaces between two words. The returned string should only have 
+// a single space separating the words. Do not 
+// include any extra spaces.
+
+// Example 1:
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+
+// Example 2:
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain 
+// leading or trailing spaces.
+
+// Example 3:
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words 
+// to a single space in the reversed string.
+
+// Example 4:
+// Input: s = "  Bob    Loves  Alice   "
+// Output: "Alice Loves Bob"
+
+// Example 5:
+// Input: s = "Alice does not even like bob"
+// Output: "bob like even not does Alice"
+ 
+// Constraints:
+// 1 <= s.length <= 104
+// s contains English letters (upper-case and lower-case),
+//  digits, and spaces ' '.
+// There is at least one word in s.
+ 
+
+// Follow-up: If the string data type is mutable in your language,
+//  can you solve it in-place with O(1) extra space?
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string intermediate;
+
+        stringstream check1(s);
+        vector<string> tokens;
+        while(getline(check1,intermediate,' ')){
+            tokens.push_back(intermediate);
+        }
+
+        reverse(tokens.begin(),tokens.end());
+        string ans = "";
+        for(int i=0;i<tokens.size();i++){
+            string newString;
+            for(int j=0;j<tokens[i].size();j++){
+                if(tokens[i][j] != ' '){
+                    newString.push_back(tokens[i][j]);
+                }
+            }
+            tokens[i] = newString;
+            if(newString != ""){
+                ans += newString + " ";    
+            }
+             
+        }
+        return ans.substr(0,ans.length() - 1);
+    }
+};
