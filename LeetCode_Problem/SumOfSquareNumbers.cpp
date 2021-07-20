13bd7372ffcc2955b2ad937c97d22f9b657a7a01
goel-aman <amangoel9873572693@gmail.com>
2021-07-20 15:36:49 +0530

    SumOfSquareNumbers.cpp

---
+++ LeetCode_Problem/SumOfSquareNumbers.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 633. Sum of Square Numbers
+// Given a non-negative integer c, decide whether there're
+//  two integers a and b such that a2 + b2 = c.
+
+// Example 1:
+// Input: c = 5
+// Output: true
+// Explanation: 1 * 1 + 2 * 2 = 5
+
+// Example 2:
+// Input: c = 3
+// Output: false
+
+// Example 3:
+// Input: c = 4
+// Output: true
+
+// Example 4:
+// Input: c = 2
+// Output: true
+
+// Example 5:
+// Input: c = 1
+// Output: true
+ 
+// Constraints:
+// 0 <= c <= 231 - 1
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(int i=0;i<=sqrt(c);i++){
+            int value = c - (i*i);
+            
+            int val = sqrt(value);
+            if(c == (val * val)  + (i * i)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
