1b924990187ae5c3aa88bde98c1006548699bc66
goel-aman <amangoel9873572693@gmail.com>
2021-08-15 15:44:44 +0530

    MinimumCostTreeFromLeafValues.cpp

--- LeetCode_Problem/MinimumCostTreeFromLeafValues.cpp
+++ LeetCode_Problem/MinimumCostTreeFromLeafValues.cpp
@@ -35,3 +35,39 @@ using namespace std;
 // It is guaranteed that the answer fits into a 32-bit 
 // signed integer (ie. it is less than 2^31).
 
+
+
+class Solution {
+    unordered_map<string,pair<int,int>> um;
+public:
+    // first is maxLeafValue, second add
+    pair<int,int> solve(vector<int>& arr,int start,int end){
+        if(start == end){
+            return {arr[start],0};
+        }
+        
+        string input = to_string(start) + ";" + to_string(end);
+        if(um.count(input) > 0){
+            return um[input];
+        }
+
+        int maxLeaf = INT_MIN;
+        int sum = INT_MAX;
+
+        for(int i=start;i<end;i++){
+            pair<int,int> left = solve(arr,start,i);
+            pair<int,int> right = solve(arr,i + 1,end);
+            maxLeaf = max(maxLeaf,max(left.first,right.first));
+            sum = min(sum,left.second + right.second + left.first * right.first);
+        }
+        
+        um[input] = {maxLeaf,sum};
+        return {maxLeaf,sum};
+    }
+
+    int mctFromLeafValues(vector<int>& arr) {
+        int arrLength = arr.size();
+        pair<int,int> ans = solve(arr,0,arrLength - 1);
+        return ans.second;      
+    }
+};
