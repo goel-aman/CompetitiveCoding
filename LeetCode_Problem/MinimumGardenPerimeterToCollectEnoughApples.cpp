ad5382a388886922fb12b18853e666049db89b77
goel-aman <amangoel9873572693@gmail.com>
2021-08-19 15:50:49 +0530

    MinimumGardenPerimeterToCollectEnoughApples.cpp

--- LeetCode_Problem/MinimumGardenPerimeterToCollectEnoughApples.cpp
+++ LeetCode_Problem/MinimumGardenPerimeterToCollectEnoughApples.cpp
@@ -2,36 +2,49 @@
 using namespace std;
 
 // 1954. Minimum Garden Perimeter to Collect Enough Apples
+// In a garden represented as an infinite 2D grid, there is an apple
+//  tree planted at every integer coordinate. The apple tree planted 
+// at an integer coordinate (i, j) has |i| + |j| apples growing on it.
 
-// In a garden represented as an infinite 2D grid, there is an apple tree planted at every integer coordinate. The apple tree planted at an integer coordinate (i, j) has |i| + |j| apples growing on it.
+// You will buy an axis-aligned square plot of land that is centered 
+// at (0, 0).
 
-// You will buy an axis-aligned square plot of land that is centered at (0, 0).
-
-// Given an integer neededApples, return the minimum perimeter of a plot such that at least neededApples apples are inside or on the perimeter of that plot.
+// Given an integer neededApples, return the minimum perimeter of a plot 
+// such that at least neededApples apples are inside or on the perimeter
+//  of that plot.
 
 // The value of |x| is defined as:
-
 // x if x >= 0
 // -x if x < 0
  
-
 // Example 1:
-
-
 // Input: neededApples = 1
 // Output: 8
 // Explanation: A square plot of side length 1 does not contain any apples.
-// However, a square plot of side length 2 has 12 apples inside (as depicted in the image above).
+// However, a square plot of side length 2 has 12 apples inside
+//  (as depicted in the image above).
 // The perimeter is 2 * 4 = 8.
-// Example 2:
 
+// Example 2:
 // Input: neededApples = 13
 // Output: 16
-// Example 3:
 
+// Example 3:
 // Input: neededApples = 1000000000
 // Output: 5040
  
-
 // Constraints:
 // 1 <= neededApples <= 1015
+
+
+class Solution {
+public:
+    long long minimumPerimeter(long long neededApples) {
+        long long int i = 1;
+        for(;neededApples > 0;i++){
+            neededApples -= (12 * i);
+            neededApples -= (((2*i - 1)*(2*i)/2) - ((i)*(i + 1)/2)) * 8; 
+        }
+        return (i - 1) * 8;
+    }
+};
