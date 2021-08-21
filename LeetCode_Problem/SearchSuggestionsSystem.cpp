8c7b436fdeab897664a5b7af904ee719473b0834
goel-aman <amangoel9873572693@gmail.com>
2021-08-21 11:33:08 +0530

    SearchSuggestionSystem.cpp

---
+++ LeetCode_Problem/SearchSuggestionsSystem.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1268. Search Suggestions System
+// Given an array of strings products and a string 
+// searchWord. We want to design a system that suggests
+//  at most three product names from products after each
+//  character of searchWord is typed. Suggested products 
+// should have common prefix with the searchWord. If there
+// are more than three products with a common prefix return
+//  the three lexicographically minimums products.
+// Return list of lists of the suggested products after 
+// each character of searchWord is typed. 
+
+// Example 1:
+// Input: products = ["mobile","mouse","moneypot","monitor",
+// "mousepad"], searchWord = "mouse"
+// Output: [
+// ["mobile","moneypot","monitor"],
+// ["mobile","moneypot","monitor"],
+// ["mouse","mousepad"],
+// ["mouse","mousepad"],
+// ["mouse","mousepad"]
+// ]
+// Explanation: products sorted lexicographically = ["mobile"
+// ,"moneypot","monitor","mouse","mousepad"]
+// After typing m and mo all products match and we show 
+// user ["mobile","moneypot","monitor"]
+// After typing mou, mous and mouse the system 
+// suggests ["mouse","mousepad"]
+
+// Example 2:
+// Input: products = ["havana"], searchWord = "havana"
+// Output: [["havana"],["havana"],["havana"],["havana"],
+// ["havana"],["havana"]]
+
+// Example 3:
+// Input: products = ["bags","baggage","banner","box","cloths"],
+//  searchWord = "bags"
+// Output: [["baggage","bags","banner"],["baggage","bags","banner"],
+// ["baggage","bags"],["bags"]]
+
+// Example 4:
+// Input: products = ["havana"], searchWord = "tatiana"
+// Output: [[],[],[],[],[],[],[]]
+ 
+// Constraints:
+// 1 <= products.length <= 1000
+// There are no repeated elements in products.
+// 1 <= Σ products[i].length <= 2 * 10^4
+// All characters of products[i] are lower-case English letters.
+// 1 <= searchWord.length <= 1000
+// All characters of searchWord are lower-case English letters.
+
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(),products.end());
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> um;
+        int productsLength = products.size();
+        int searchWordLength = searchWord.length();
+        string val = "";
+        for(int j=0;j<searchWordLength;j++){
+            val += searchWord[j];
+            vector<string> gotIt;
+            int count = 0;
+            for(int i=0;i<productsLength;i++){
+                string element = products[i];
+                if(element.length() >= val.length()){
+                    if(element.substr(0,val.length()) == val){
+                        gotIt.push_back(element);
+                        count++;
+                    }
+                }
+                if(count >= 3){
+                    break;
+                }
+            }
+            ans.push_back(gotIt);
+        }
+        return ans;
+    }
+};
