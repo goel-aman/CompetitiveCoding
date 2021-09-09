8fffbbb5b62e5c5ed72bc52a287f9cd9ef8045e4
goel-aman <amangoel9873572693@gmail.com>
2021-09-09 19:57:52 +0530

    CommentsAdded

--- LeetCode_Problem/MaximumSwap.cpp
+++ LeetCode_Problem/MaximumSwap.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// 670. Maximum Swap
+// You are given an integer num. You can swap two
+//  digits at most once to get the maximum valued number.
+// Return the maximum valued number you can get.
+
+// Example 1:
+// Input: num = 2736
+// Output: 7236
+// Explanation: Swap the number 2 and the number 7.
+
+// Example 2:
+// Input: num = 9973
+// Output: 9973
+// Explanation: No swap.
+ 
+// Constraints:
+// 0 <= num <= 108
 
 bool compare(char& a,char &b){
     if(a > b){
