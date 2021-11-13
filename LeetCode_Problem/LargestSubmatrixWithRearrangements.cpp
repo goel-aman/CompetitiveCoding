c14c5c89aeb99998fd07bbba3c9e5bd031ad575d
goel-aman <amangoel9873572693@gmail.com>
2021-11-13 08:32:25 +0530

    LargestSubmatrixWithRearrangements.cpp

---
+++ LeetCode_Problem/LargestSubmatrixWithRearrangements.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 1727. Largest Submatrix With Rearrangements
+// You are given a binary matrix matrix of size m x n, 
+// and you are allowed to rearrange the columns of the
+//  matrix in any order.
+
+// Return the area of the largest submatrix within matrix
+//  where every element of the submatrix is 1 after 
+// reordering the columns optimally.
+
+// Example 1:
+// Input: matrix = [[0,0,1],[1,1,1],[1,0,1]]
+// Output: 4
+// Explanation: You can rearrange the columns as shown above.
+// The largest submatrix of 1s, in bold, has an area of 4.
+
+// Example 2:
+// Input: matrix = [[1,0,1,0,1]]
+// Output: 3
+// Explanation: You can rearrange the columns as shown above.
+// The largest submatrix of 1s, in bold, has an area of 3.
+
+// Example 3:
+// Input: matrix = [[1,1,0],[1,0,1]]
+// Output: 2
+// Explanation: Notice that you must rearrange entire columns,
+//  and there is no way to make a submatrix of 1s larger than
+//  an area of 2.
+
+// Example 4:
+// Input: matrix = [[0,0],[0,0]]
+// Output: 0
+// Explanation: As there are no 1s, no submatrix of 1s can 
+// be formed and the area is 0.
+ 
+// Constraints:
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m * n <= 105
+// matrix[i][j] is 0 or 1.
+
+
+class Solution {
+public:
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> tempMat(rows,vector<int> (cols,0));
+        for(int i=0;i<cols;i++){
+            int sum = 0;
+            for(int j=0;j<rows;j++){
+                if(matrix[j][i] == 0){
+                    sum = 0;
+                }else{
+                    sum += 1;
+                }
+                tempMat[j][i] = sum;
+            }
+        }      
+        int ans = 0;
+        for(int i=0;i<rows;i++){
+            sort(tempMat[i].begin(),tempMat[i].end());
+            int length = 0;
+            int value = 0;
+            for(int j=cols - 1;j>=0;j--){
+                if(tempMat[i][j] == 0){
+                    length = 0;
+                    value = 0;
+                    break;
+                    // continue;
+                }else{
+                    length++;
+                    value = (length * tempMat[i][j]);
+                }
+                ans = max(ans,value);
+            }
+        }
+        return ans;
+    }
+};
