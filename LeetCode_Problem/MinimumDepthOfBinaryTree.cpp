86df4eb479df9bfb458062da30ac6d0bba0d969f
goel-aman <amangoel9873572693@gmail.com>
2021-12-25 07:35:40 +0530

    PathSum.cpp

--- LeetCode_Problem/MinimumDepthOfBinaryTree.cpp
+++ LeetCode_Problem/MinimumDepthOfBinaryTree.cpp
@@ -20,17 +20,15 @@ using namespace std;
 // The number of nodes in the tree is in the range [0, 105].
 // -1000 <= Node.val <= 1000
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
     int minDepth(TreeNode* root) {
