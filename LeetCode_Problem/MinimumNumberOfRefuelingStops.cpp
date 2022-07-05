9833b60f7f4ca3de6ed50e21125ff72a48da3d67
goel-aman <amangoel9873572693@gmail.com>
2022-07-05 15:39:33 +0530

    MinimumNumberOfRefuelingStops.cpp

---
+++ LeetCode_Problem/MinimumNumberOfRefuelingStops.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 871. Minimum Number of Refueling Stops
+// A car travels from a starting position to a destination which
+//  is target miles east of the starting position.
+// There are gas stations along the way. The gas stations are 
+// represented as an array stations where stations[i] = [positioni, fueli] 
+// pindicates that the ith gas station is positioni miles east of the 
+// starting position and has fueli liters of gas.
+// The car starts with an infinite tank of gas, which initially has startFuel
+// liters of fuel in it. It uses one liter of gas per one mile that it drives.
+// When the car reaches a gas station, it may stop and refuel, transferring
+// all the gas from the station into the car.
+
+// Return the minimum number of refueling stops the car must make in order to
+//  reach its destination. If it cannot reach the destination, return -1.
+
+// Note that if the car reaches a gas station with 0 fuel left, the car can 
+// still refuel there. If the car reaches the destination with 0 fuel left, it
+// is still considered to have arrived.
+
+// Example 1:
+// Input: target = 1, startFuel = 1, stations = []
+// Output: 0
+// Explanation: We can reach the target without refueling.
+
+// Example 2:
+// Input: target = 100, startFuel = 1, stations = [[10,100]]
+// Output: -1
+// Explanation: We can not reach the target (or even the first gas station).
+
+// Example 3:
+// Input: target = 100, startFuel = 10, stations = [[10,60],[20,30],[30,30],[60,40]]
+// Output: 2
+// Explanation: We start with 10 liters of fuel.
+// We drive to position 10, expending 10 liters of fuel.  We refuel from 0 
+// liters to 60 liters of gas.
+// Then, we drive from position 10 to position 60 (expending 50 liters of fuel),
+// and refuel from 10 liters to 50 liters of gas.  We then drive to and 
+// preach the target.
+// We made 2 refueling stops along the way, so we return 2.
+ 
+// Constraints:
+// 1 <= target, startFuel <= 109
+// 0 <= stations.length <= 500
+// 0 <= positioni <= positioni+1 < target
+// 1 <= fueli < 109
+
+
+class Solution {
+public:
+    int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
+        int stationsLength = stations.size();
+        int position = startFuel;
+        int count = 0;
+        int index = 0;
+        priority_queue<int> pq;
+        
+        for(int i=index;i<stationsLength;i++){
+            if(stations[i][0] <= position){
+                pq.push(stations[i][1]);
+                index = i;
+            }
+        }
+
+        while(true){
+            if(position >= target){
+                return count;
+            }  
+
+            if(pq.empty()){
+                return -1;
+            }
+
+            int top = pq.top();
+            pq.pop();
+            position += top;
+
+            for(int i=index + 1;i<stationsLength;i++){
+               if(stations[i][0] <= position){
+                    pq.push(stations[i][1]);
+                    index = i;
+                }
+            }
+
+            count++;
+        }
+        return count;
+    }
+};
